d1fc16fd8c448d46bed74f6cec8e5f9b191f3d33
Nguyen Truong Thinh <nguyentruongthinhvn2020@gmail.com>
2022-03-19 00:11:04 +0700

    List Tasks Maker App: Complete!

--- README.md
+++ README.md
@@ -5,7 +5,12 @@ ___
 "Android mobile development has been Kotlin-first since Google I/O in 2019"<br>
 I follow the latest trends & technology in the market. <br>I make sure my code is of production quality.
 ___
-#### #1: TimeFighter
-This is a pretty simple but quality app.<br>The app allows users to touch a button for a period of 60 seconds with friendly animations.<br>Currently, the application supports 2 languages: English and Vietnamese.
+#### #1: Time Fighter
+This is a pretty simple but quality app.<br>The app allows users to touch a button for a period of 60 seconds with friendly animations.<br>Currently, the application supports 2 languages: English and Vietnamese.<br>
+#### #2: List Tasks Maker
+The app allows users to create to-do tasks list.<br>
+For each task, there will be its own list of tasks.<br>
+The app supports both tablet and phone interfaces.<br>
+Currently, the application supports 2 languages: English and Vietnamese.<br>
 ___
 ### License: MIT
--- app/src/main/java/com/forever/bee/listtasksmaker/MainActivity.kt
+++ app/src/main/java/com/forever/bee/listtasksmaker/MainActivity.kt
@@ -13,6 +13,8 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.text.InputType
+import android.view.Menu
+import android.view.MenuItem
 import android.widget.EditText
 import androidx.appcompat.app.AlertDialog
 import androidx.core.os.bundleOf
@@ -78,6 +80,29 @@ class MainActivity : AppCompatActivity(), MainFragment.MainFragmentInteractionLi
 
     override fun listItemTapped(list: TaskList) {
         showListDetail(list)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        super.onCreateOptionsMenu(menu)
+        menuInflater.inflate(R.menu.menu_info, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (item.itemId == R.id.about_item) {
+            showInfo()
+        }
+
+        return true
+    }
+
+    private fun showInfo() {
+        val dialogTitle = getString(R.string.about_title, BuildConfig.VERSION_NAME)
+        val dialogMessage = getString(R.string.about_message)
+        val builder = AlertDialog.Builder(this)
+        builder.setTitle(dialogTitle)
+        builder.setMessage(dialogMessage)
+        builder.create().show()
     }
 
     private fun showCreateListDialog() {
